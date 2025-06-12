a9a5e0f58bce0732f964bacb0ebfae5cefb1d4f8
Bartash Oksana <kasandraoks84@gmail.com>
2025-06-12 23:21:48 +0300

    Changed_header_in_Lesson-19

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -2,8 +2,35 @@ document.addEventListener('click', documentActions)
 
 function documentActions(e) {
 	const targetElement = e.target
+
+	// Відкриття/закриття меню при кліку на бургер-іконку
 	if (targetElement.closest('.header__icon-menu')) {
 		document.body.classList.toggle('menu-open')
 	}
 
+		// Закриття меню та плавний скролл при натисканні на пункт меню
+	if (targetElement.classList.contains('header__link')) {
+		// Закриваємо меню
+		document.body.classList.remove('menu-open');
+
+		// Отримуємо href (ідентифікатор блоку)
+		const goTo = targetElement.getAttribute('href');
+
+		// Знаходимо цей блок на сторінці
+		const goToElement = document.querySelector(goTo);
+
+		// Висота хедера, щоб не перекрив блок
+		const headerHeight = document.querySelector('.header').offsetHeight;
+
+		if (goToElement) {
+			// Плавна прокрутка
+			window.scrollTo({
+				top: goToElement.offsetTop - headerHeight - 10,
+				behavior: 'smooth'
+			});
+		}
+
+		// Забороняємо стандартну поведінку (перехід за посиланням)
+		e.preventDefault();
+	}
 }
