df6f232270dc2c3b302fc96868f03c56bbd6ebcb
Bartash Oksana <kasandraoks84@gmail.com>
2025-06-12 23:08:28 +0300

    Changed_header_in_lesson-16

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -2,13 +2,45 @@ document.addEventListener('click', documentActions)
 
 function documentActions(e) {
 	const targetElement = e.target
+	//Відкриття і закриття підпунктів у меню в ecobazar.html
 	if (targetElement.closest('.menu__arrow')) {
 		targetElement.closest('.menu__item').classList.toggle('active')
 	}
+
+	// Відкриття/закриття меню при кліку на бургер-іконку в ecobazar.html
 	if (targetElement.closest('.icon-menu')) {
 		document.body.classList.toggle('menu-open')
 	}
-		if (targetElement.closest('.business-header__menu-icon')) {
+
+	// Відкриття/закриття меню при кліку на бургер-іконку в business/index.html
+	if (targetElement.closest('.business-header__menu-icon')) {
 		document.body.classList.toggle('menu-open-2')
+	}
+
+
+	// Закриття меню та плавний скролл при натисканні на пункт меню
+	if (targetElement.classList.contains('business-header__link')) {
+		// Закриваємо меню
+		document.body.classList.remove('menu-open-2');
+
+		// Отримуємо href (ідентифікатор блоку)
+		const goTo = targetElement.getAttribute('href');
+
+		// Знаходимо цей блок на сторінці
+		const goToElement = document.querySelector(goTo);
+
+		// Висота хедера, щоб не перекрив блок
+		const headerHeight = document.querySelector('.business-header').offsetHeight;
+
+		if (goToElement) {
+			// Плавна прокрутка
+			window.scrollTo({
+				top: goToElement.offsetTop - headerHeight - 10,
+				behavior: 'smooth'
+			});
+		}
+
+		// Забороняємо стандартну поведінку (перехід за посиланням)
+		e.preventDefault();
 	}
 }
